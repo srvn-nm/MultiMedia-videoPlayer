7af9728dbea0011a1ecd83ced557725e50829721
sarvin nami <srvn0nm@gmail.com>
2024-06-30 13:36:03 +0330

    debug server

--- my-video-platform/server.js
+++ my-video-platform/server.js
@@ -1,44 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
-const { Sequelize, DataTypes } = require("sequelize");
-const tf = require("@tensorflow/tfjs-node");
+const bodyParser = require("body-parser");
 const toxicity = require("@tensorflow-models/toxicity");
+const tf = require("@tensorflow/tfjs-node");
 
 const app = express();
 const port = 3000;
 
-// Database setup
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "database.sqlite",
-});
-
-const Comment = sequelize.define(
-  "Comment",
-  {
-    videoTitle: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    text: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    sentiment: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
-
-// sequelize.sync();
-
 // Middleware to parse JSON requests
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Serve static files from the parent directory
 app.use(express.static(path.join(__dirname, "..")));
@@ -48,12 +18,9 @@ app.get("/", (req, res) => {
 });
 
 // Load the toxicity model
-let toxicityModel = null;
+let toxicityModel;
 toxicity.load(0.9).then((model) => {
   toxicityModel = model;
-  console.log("Toxicity model loaded.");
-}).catch((err) => {
-  console.error("Failed to load the toxicity model:", err);
 });
 
 // Function to analyze comments
@@ -93,43 +60,6 @@ app.post("/analyze", async (req, res) => {
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
-});
-
-// Endpoint to add a new comment
-app.post("/addComment", async (req, res) => {
-  const { videoTitle, commentText } = req.body;
-
-  if (!commentText) {
-    return res.status(400).json({ error: "Comment text is required" });
-  }
-
-  try {
-    const sentimentResults = await analyzeComments([commentText]);
-    const newComment = await Comment.create({
-      videoTitle,
-      text: commentText,
-      sentiment: sentimentResults[0].sentiment,
-    });
-
-    res.json(newComment);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// Endpoint to get comments for a video
-app.get("/comments", async (req, res) => {
-  const { videoTitle } = req.query;
-
-  try {
-    const comments = await Comment.findAll({
-      where: { videoTitle },
-      order: [['createdAt', 'DESC']],
-    });
-    res.json(comments);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
 });
 
 app.listen(port, () => {
